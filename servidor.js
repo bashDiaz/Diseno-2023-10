802327b65101b0451758ee7ad98b9286ed109159
josefervv <joseloplexx@gmail.com>
2023-03-14 10:08:04 -0500

    Actualizacion

--- servidor.js
+++ servidor.js
@@ -1,100 +1,35 @@
-// Import the dgram module
 const dgram = require('dgram');
-
-// Create a UDP server using the 'udp4' protocol
 const server = dgram.createSocket('udp4');
-
 const express = require('express')
 const app = express()
-app.use(express.static(__dirname + "/static"));
+const mysql = require('mysql');
 
-// Initialize an empty array to hold information received from UDP messages
+app.use(express.static(__dirname + "/static"));
 let info = [];
 
-// Listen for incoming messages on the server
+// Listen for incoming messages on the server and adding to a server
 server.on('message', (msg, rinfo) => {
-  // Convert the message to a string
   const data = msg.toString();
-  
-  // Split the string into an array of fields, separated by ';'
   const fields = data.split(';');
-  
-  // Update the 'info' array with the new fields
   info = fields;
-  
-  // Create a new array with only the first 4 fields
   const dataToInsert = [fields[0], fields[1], fields[2], fields[3]];
-  
-  // Insert the data into a database
   insertData(dataToInsert);
 });
 
 // Listen for the server to start listening
 server.on('listening', () => {
-  // Get the address and port of the server
   const address = server.address();
-  
-  // Print the address and port to the console
   console.log(`UDP server listening on ${address.address}:${address.port}`);
 });
-
-// Bind the server to port 1234
 server.bind(1234);
-
-// Import the http and fs modules
-/*const http = require('http');
-const fs = require('fs');
-
-// Create an HTTP server
-const serverHttp = http.createServer((req, res) => {
-  // If the requested URL is '/data'
-  if (req.url === '/data') {
-    // Set the response headers to indicate plain text
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    
-    // Write the 'info' array to the response, joined by ';'
-    res.write(info.join(';'));
-    
-    // End the response
-    res.end();
-  } else {
-    // Otherwise, read the 'index.html' file
-    fs.readFile('index.html', (err, data) => {
-      // If there was an error reading the file
-      if (err) {
-        // Set the response status code to 404
-        res.writeHead(404);
-        
-        // Write a message to the response
-        res.write('File not found!');
-        
-        // End the response
-        res.end();
-      } else {
-        // Otherwise, set the response headers to indicate HTML
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        
-        // Write the file data to the response
-        res.write(data);
-        
-        // End the response
-        res.end();
-      }
-    });
-  }
-});*/
 
 // Listen for incoming requests on port 80
 app.listen(80, () => {
   console.log('HTTP server listening on port 80');
 });
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
-
-// Import the mysql module
-const mysql = require('mysql');
 
 // Create a new MySQL database connection
 const connection = mysql.createConnection({
@@ -115,14 +50,8 @@ connection.connect((error) => {
 
 // Define a function to insert data into the MySQL database
 function insertData(data) {
-
-  // Create an array with the data to insert
   const datos = [info[0], info[1], info[2], info[3]];
-  
-  // Define an SQL query to insert the data
   const sql = "INSERT INTO datos_gps (Latitud, Longitud, Fecha, Hora) VALUES (?, ?, ?, ?)";
-  
-  // Execute the SQL query with the data and log the results
   connection.query(sql, datos, (error, results, fields) => {
     if (error) {
       console.log("Error inserting data into MySQL database: " + error);
--- static/maping.js
+++ static/maping.js
@@ -4,16 +4,28 @@ let mymap = L.map('map').setView([0, 0], 50);
 // Add a tile layer to the map using OpenStreetMap's tile server
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-  maxZoom: 18, // Set the maximum zoom level
-}).addTo(mymap); // Add the tile layer to the map
+  maxZoom: 18,
+}).addTo(mymap); 
 
-// Add a marker to the map at a specific latitude and longitude
+const markers = {};
 function handleUdpData(data) {
-  const [lat, lng] = data.split(';');
+  // Add a marker to the map at a specific latitude and longitude
+  const [lat, lng, seq] = data.split(';');
   let newCoords = L.latLng(lat, lng);
   L.marker(newCoords).addTo(mymap);
-  // Add diferent location for the polyline (please select points to track)
-  polylineCoords.push(L.latLng(lat, lng));
-  // Add a polyline to the map
-  L.polyline(polylineCoords, {color: 'red'}).addTo(mymap);
+  // Add polylines for markers registered on UDP sender and based on sequences
+  if (!markers[seq]) {
+    markers[seq] = [];
+  }
+  markers[seq].push(newCoords);
+  const latLngs = markers[seq];
+  if (latLngs.length > 1) {
+    if (!markers[seq].polyline) {
+      const polyline = L.polyline(latLngs).addTo(mymap);
+      markers[seq] = polyline;
+    } else {
+      markers[seq].addLatLng(newCoords);
+      markers[seq].setLatLngs(markers[seq].getLatLngs());
+    }
+  }
 }
