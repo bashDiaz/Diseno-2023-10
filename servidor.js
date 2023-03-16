78047415f84c602eae1c19a99853e6bc9a8a4bcc
josefervv <joseloplexx@gmail.com>
2023-03-15 23:21:02 -0500

    Agregando Infraestructura

--- servidor.js
+++ servidor.js
@@ -1,67 +1,67 @@
+const express = require('express');
 const dgram = require('dgram');
+const app = express();
 const server = dgram.createSocket('udp4');
-const express = require('express')
-const app = express()
 const mysql = require('mysql');
 
-app.use(express.static(__dirname + "/static"));
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
-let info = [];
+let data1 = null;
+let data2 = null;
+let data3 = null;
+let data4 = null;
 
-// Listen for incoming messages on the server and adding to a server
-app.use(express.static('public'));
-function updateMap(lat, lng) {
-  // Code to update the map with the new latitude and longitude
-}
 server.on('message', (msg, rinfo) => {
-  const data = msg.toString();
-const fields = data.split(';');
-  info = fields;
-  const dataToInsert = [fields[0], fields[1], fields[2], fields[3]];
-  insertData(dataToInsert);
- insertData(dataToInsert);
-const lat = parseFloat(fields[0]);
-  const lon = parseFloat(fields[1]);
-  updateMap(lat, lon);
-
-});
-app.get('/data', (req, res) => {
-res.json(info);
+  const data = msg.toString().split(';');
+  const data1 = parseFloat(data[0]);
+  const data2 = parseFloat(data[1]);
+  const data3 = parseFloat(data[2]);
+  const data4 = parseFloat(data[3]);
+  console.log(`Data received: ${data1}, ${data2}, ${data3}, ${data4}`);
+  document.getElementById('data').innerHTML = result;
 });
 
-// Listen for the server to start listening
 server.on('listening', () => {
   const address = server.address();
   console.log(`UDP server listening on ${address.address}:${address.port}`);
 });
+
 server.bind(1234);
 
-// Listen for incoming requests on port 80
-app.listen(80, () => {
-  console.log('HTTP server listening on port 80');
-});
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-const port = 80;
-app.listen(port, () => {
-  console.log(`Servidor iniciado en el puerto ${port}`);
+
+app.use(express.static(_dirname + "/static"));
+
+app.get('/data', (req, res) => {
+  res.json({
+    data1: data1,
+    data2: data2,
+    data3: data3,
+    data4: data4
+  });
 });
 
-// Create a new MySQL database connection
+serverHttp.listen(80, () => {
+  console.log('HTTP server listening on port 80');
+});
+
+// Refresh data every 10 seconds
+setInterval(() => {
+  server.send('refresh', 0, 'refresh'.length, 3333, 'localhost', (err, bytes) => {
+    if (err) {
+      console.log(`Error refreshing data: ${err.message}`);
+    } else {
+      console.log(`Data refreshed`);
+    }
+  });
+}, 10000);
+
 const connection = mysql.createConnection({
   host: 'mysql1.czemchtiopw1.us-east-1.rds.amazonaws.com',
   user: 'admin',
   password: 'prueba123',
   database: 'mysql1'
 });
-
-// Connect to the database
 connection.connect((error) => {
   if (error) {
     console.error('Error connecting to MySQL database: ' + error.stack);
@@ -70,15 +70,14 @@ connection.connect((error) => {
   console.log('Connected to MySQL database with id ' + connection.threadId);
 });
 
-// Define a function to insert data into the MySQL database
 function insertData(data) {
-  const datos = [info[0], info[1], info[2], info[3]];
-  const sql = "INSERT INTO datos_gps (Latitud, Longitud, Fecha, Hora) VALUES (?, ?, ?, ?)";
-  connection.query(sql, datos, (error, results, fields) => {
-    if (error) {
-      console.log("Error inserting data into MySQL database: " + error);
-    } else {
-      console.log("Data inserted successfully!");
-    }
-  });
+const datos = [info[0], info[1], info[2], info[3]];
+const sql = "INSERT INTO datos_gps (Latitud, Longitud, Fecha, Hora) VALUES (?, ?, ?, ?)";
+connection.query(sql, datos, (error, results, fields) => {
+  if (error) {
+    console.log("Error inserting data into MySQL database: " + error);
+  } else {
+    console.log("Data inserted successfully!");
+  }
+});
 }
--- static/udp.js
+++ static/udp.js
@@ -1,31 +1,20 @@
 // Define a function called "updateData" that will be used to fetch new data from the server and update the page
 function updateData() {
-  // Create a new XMLHttpRequest object
   const req = new XMLHttpRequest();
-  // Define a callback function to execute when the XMLHttpRequest object changes state
   req.onreadystatechange = function() {
-    // If the XMLHttpRequest object has completed its request and received a successful response...
     if (this.readyState === 4 && this.status === 200) {
-      // Split the response text into an array of strings using semicolons as delimiters
       const data = this.responseText.split(';');
-      // Get a reference to the <ul> element on the page with an ID of "data"
       const ul = document.getElementById('data');
-      // Clear the <ul> element of any existing child nodes
       ul.innerHTML = '';
-      // Loop through the data array in reverse order and create a new <li> element for each string
       for (let i = data.length - 1; i >= 0; i--) {
         const li = document.createElement('li');
         li.appendChild(document.createTextNode(data[i]));
         ul.appendChild(li);
       }
-      // Create a table with data
-
     }
   };
   // Open a GET request to the "/data" endpoint on the server
   req.open('GET', '/data', true);
-  // Send the request
   req.send();
 }
-// Call the "updateData" function every 10 seconds using the setInterval method
 setInterval(updateData, 10000);
