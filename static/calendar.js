ae1c1db12e54b50e475a89d7f9cb1c200e08c349
samuelrs7 <rsrojas@uninorte.edu.co>
2023-03-20 22:06:58 -0500

    Update

--- static/calendar.js
+++ static/calendar.js
@@ -1,25 +1,33 @@
-app.get('/linea', (req, res) => {
-  // Obtiene los valores de fecha y hora del query
-  const fechaInicio = req.query.fecha_inicio;
-  const horaInicio = req.query.hora_inicio;
-  const fechaFin = req.query.fecha_fin;
-  const horaFin = req.query.hora_fin;
+const formulario = document.getElementById('formulario-fecha-hora');
 
-  // Crea la consulta SQL con los parámetros de fecha y hora
-  const query = `SELECT Latitud, Longitud FROM datos_gps WHERE Fecha >= '${fechaInicio} ${horaInicio}:00' AND Fecha <= '${fechaFin} ${horaFin}:00' ORDER BY id DESC LIMIT 50`;
+// Agrega un controlador de eventos para el envío del formulario
+formulario.addEventListener('submit', (event) => {
+  // Previene el envío del formulario por defecto
+  event.preventDefault();
 
-  connection.query(query, (error, rows) => {
-    if (error) {
-      console.error('Error al hacer el query: ', error);
-      res.status(500).send('Error al hacer el query');
-    } else {
-      console.log('Resultados del query: ', rows);
+  // Obtiene los valores de fecha y hora del formulario
+  const fechaInicio = document.getElementById('fecha-inicio').value;
+  const horaInicio = document.getElementById('hora-inicio').value;
+  const fechaFin = document.getElementById('fecha-fin').value;
+  const horaFin = document.getElementById('hora-fin').value;
 
-      const values = rows.map(obj => [parseFloat(obj.Latitud), parseFloat(obj.Longitud)]);
+  // Crea una instancia del objeto XMLHttpRequest
+  const xhr = new XMLHttpRequest();
 
-      res.json({
-        rows: values
-      });
-    }
+  // Configura la solicitud HTTP GET con los parámetros de fecha y hora
+  const url = `/linea?fecha_inicio=${fechaInicio}&hora_inicio=${horaInicio}&fecha_fin=${fechaFin}&hora_fin=${horaFin}`;
+  xhr.open('GET', url);
+
+  // Agrega un controlador de eventos para la respuesta de la solicitud
+  xhr.addEventListener('load', () => {
+    // Analiza los datos de respuesta JSON
+    const response = JSON.parse(xhr.responseText);
+    const rows = response.rows;
+
+    // Hace algo con los valores de "Latitud" y "Longitud"
+    console.log(rows);
   });
+
+  // Envía la solicitud HTTP GET
+  xhr.send();
 });
