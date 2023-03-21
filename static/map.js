f195ef0d4765ed5c29e22576c07effe9088ec006
root <root@ip-172-31-48-18.ec2.internal>
2023-03-20 20:08:10 -0500

    Mapa con polilinea

--- static/map.js
+++ static/map.js
@@ -1,5 +1,9 @@
 // Create a map object and set its view to a specific latitude, longitude, and zoom level
+const data1=11.02713824366;
+const data2=-74.82960349742;
+vector=[[11.02713824366,-74.82960349742]];
 let mymap = L.map('map').setView([data1, data2], 12);
+polyline = L.polyline(vector, {color: 'red'}).addTo(mymap);
 // Add a tile layer to the map using OpenStreetMap's tile server
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
@@ -12,8 +16,24 @@ function updateMarker(data1, data2) {
   marker.setLatLng([data1, data2]);
 }
 
+function updatePolyline(rows) {
+  var coordsArray =rows
+  if (polyline) {
+    polyline.setLatLngs(coordsArray);
+  } else {
+    polyline = L.polyline(coordsArray, {color: 'red'}).addTo(mymap);
+    console.log('fcn')
+  }
+}
 
-
+setInterval(() => {
+  fetch('/linea')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.rows);
+      updatePolyline(data.rows);
+    });
+}, 1000);
 
 // const markers = {};
 // function handleUdpData(data) {
