c6b28c3ddd209d791f5c604d22cfab744c21d3c7
root <root@ip-172-31-48-18.ec2.internal>
2023-03-20 21:52:15 -0500

    Calendario ok

--- static/calendar.js
+++ static/calendar.js
@@ -1,28 +1,25 @@
-const startDatePicker = flatpickr("#start-date", {
-    dateFormat: "d-m-Y",
-    minDate: "today",
-    maxDate: new Date().fp_incr(14),
-    enableTime: false,
-    onClose: function(selectedDates, dateStr, instance) {
-        if (selectedDates.length === 1) {
-            instance.setDate(selectedDates[0]);
-        }
-    }
-});
+app.get('/linea', (req, res) => {
+  // Obtiene los valores de fecha y hora del query
+  const fechaInicio = req.query.fecha_inicio;
+  const horaInicio = req.query.hora_inicio;
+  const fechaFin = req.query.fecha_fin;
+  const horaFin = req.query.hora_fin;
 
-const endDatePicker = flatpickr("#end-date", {
-    dateFormat: "d-m-Y",
-    minDate: "today",
-    maxDate: new Date().fp_incr(14),
-    enableTime: false,
-    onClose: function(selectedDates, dateStr, instance) {
-        if (selectedDates.length === 1) {
-            instance.setDate(selectedDates[0]);
-        }
-    }
-});
+  // Crea la consulta SQL con los parámetros de fecha y hora
+  const query = `SELECT Latitud, Longitud FROM datos_gps WHERE Fecha >= '${fechaInicio} ${horaInicio}:00' AND Fecha <= '${fechaFin} ${horaFin}:00' ORDER BY id DESC LIMIT 50`;
 
-document.getElementById("clear-dates").addEventListener("click", function() {
-    startDatePicker.clear();
-    endDatePicker.clear();
+  connection.query(query, (error, rows) => {
+    if (error) {
+      console.error('Error al hacer el query: ', error);
+      res.status(500).send('Error al hacer el query');
+    } else {
+      console.log('Resultados del query: ', rows);
+
+      const values = rows.map(obj => [parseFloat(obj.Latitud), parseFloat(obj.Longitud)]);
+
+      res.json({
+        rows: values
+      });
+    }
+  });
 });
