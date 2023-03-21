0391553e2376c51eee6046ab253e71e5f1c4a874
root <root@ip-172-31-48-18.ec2.internal>
2023-03-20 23:43:10 -0500

    AAA

--- static/calendar.js
+++ static/calendar.js
@@ -1,33 +1,18 @@
-const formulario = document.getElementById('formulario-fecha-hora');
+const formulario = document.querySelector('#formulario');
 
-// Agrega un controlador de eventos para el envío del formulario
 formulario.addEventListener('submit', (event) => {
-  // Previene el envío del formulario por defecto
   event.preventDefault();
+const fechaInicio = document.querySelector('#fecha_inicio').value;
+const horaInicio = document.querySelector('#hora_inicio').value;
+const fechaFin = document.querySelector('#fecha_fin').value;
+const horaFin = document.querySelector('#hora_fin').value;
 
-  // Obtiene los valores de fecha y hora del formulario
-  const fechaInicio = document.getElementById('fecha-inicio').value;
-  const horaInicio = document.getElementById('hora-inicio').value;
-  const fechaFin = document.getElementById('fecha-fin').value;
-  const horaFin = document.getElementById('hora-fin').value;
-
-  // Crea una instancia del objeto XMLHttpRequest
-  const xhr = new XMLHttpRequest();
-
-  // Configura la solicitud HTTP GET con los parámetros de fecha y hora
-  const url = `/linea?fecha_inicio=${fechaInicio}&hora_inicio=${horaInicio}&fecha_fin=${fechaFin}&hora_fin=${horaFin}`;
-  xhr.open('GET', url);
-
-  // Agrega un controlador de eventos para la respuesta de la solicitud
-  xhr.addEventListener('load', () => {
-    // Analiza los datos de respuesta JSON
-    const response = JSON.parse(xhr.responseText);
-    const rows = response.rows;
-
-    // Hace algo con los valores de "Latitud" y "Longitud"
-    console.log(rows);
-  });
-
-  // Envía la solicitud HTTP GET
-  xhr.send();
+fetch(`/linea?fecha_inicio=${fechaInicio}&hora_inicio=${horaInicio}&fecha_fin=${fechaFin}&hora_fin=${horaFin}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    // hacer algo con los datos obtenidos
+  })
+  .catch(error => console.error(error));
+  
 });
