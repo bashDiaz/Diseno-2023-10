d2b4387d5edfe8bca76a268a1e1ba39286bee624
root <root@ip-172-31-48-18.ec2.internal>
2023-03-20 23:41:03 -0500

    AA

--- static/udp.js
+++ static/udp.js
@@ -1,20 +1,23 @@
-// Define a function called "updateData" that will be used to fetch new data from the server and update the page
 function updateData() {
-  const req = new XMLHttpRequest();
-  req.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      const data = this.responseText.split(';');
-      const ul = document.getElementById('data');
-      ul.innerHTML = '';
-      for (let i = data.length - 1; i >= 0; i--) {
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(data[i]));
-        ul.appendChild(li);
-      }
-    }
-  };
-  // Open a GET request to the "/data" endpoint on the server
-  req.open('GET', '/data', true);
-  req.send();
-}
+  const data1Span = document.getElementById('data1');
+  const data2Span = document.getElementById('data2');
+  const data3Span = document.getElementById('data3');
+  const data4Span = document.getElementById('data4');
+
+  fetch('/data')
+    .then(response => response.json())
+    .then(data => {
+      data1Span.textContent = data[0];
+      data2Span.textContent = data[1];
+      data3Span.textContent = data[2];
+      data4Span.textContent = data[3];
+    })
+    .catch(error => {
+      console.log('Error al obtener los datos:', error);
+      data1Span.textContent = 'Error';
+      data2Span.textContent = 'Error';
+      data3Span.textContent = 'Error';
+      data4Span.textContent = 'Error';
+    });
+};
 setInterval(updateData, 10000);
