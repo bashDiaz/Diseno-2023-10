4b0f56d2d015d4451cbd163f60feeca8816dd4d3
josefervv <joseloplexx@gmail.com>
2023-03-20 22:52:32 -0500

    actualizacion de mapa

--- static/map.js
+++ static/map.js
@@ -1,5 +1,6 @@
 // Create a map object and set its view to a specific latitude, longitude, and zoom level
 let mymap = L.map('map').setView([data1, data2], 12);
+polyline = L.polyline(vector, {color: 'red'}).addTo(mymap);
 // Add a tile layer to the map using OpenStreetMap's tile server
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
@@ -10,30 +11,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var marker = L.marker([data1, data2]).addTo(mymap);
 function updateMarker(data1, data2) {
   marker.setLatLng([data1, data2]);
-}
+} 
+setInterval(updateMarker, 10000);
 
-
-
-
-// const markers = {};
-// function handleUdpData(data) {
-//   // Add a marker to the map at a specific latitude and longitude
-//   const [lat, lng, seq] = data.split(';');
-//   let newCoords = L.latLng(lat, lng);
-//   L.marker(newCoords).addTo(mymap);
-  // Add polylines for markers registered on UDP sender and based on sequences
-  // if (!markers[seq]) {
-  //   markers[seq] = [];
-  // }
-  // markers[seq].push(newCoords);
-  // const latLngs = markers[seq];
-  // if (latLngs.length > 1) {
-  //   if (!markers[seq].polyline) {
-  //     const polyline = L.polyline(latLngs).addTo(mymap);
-  //     markers[seq] = polyline;
-  //   } else {
-  //     markers[seq].addLatLng(newCoords);
-  //     markers[seq].setLatLngs(markers[seq].getLatLngs());
-  //   }
-  // }
-//}
+function updatePolyline(rows) {
+  var coordsArray =rows
+  if (polyline) {
+    polyline.setLatLngs(coordsArray);
+  } else {
+    polyline = L.polyline(coordsArray, {color: 'red'}).addTo(mymap);
+    console.log('fcn')
+  }
+} 
+setInterval(updatePolyline, 10000);
