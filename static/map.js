917969760882fa3b97e06fcb72c7781ad13a11ed
samuelrs7 <rsrojas@uninorte.edu.co>
2023-05-02 22:50:22 -0500

    2 Vehiculo

--- static/map.js
+++ static/map.js
@@ -11,7 +11,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
   maxZoom: 18,
 }).addTo(mymap);
-let id=1;
+let id;
 let app1Positions = [];
 let app2Positions = [];
 // Define el icono amarillo
