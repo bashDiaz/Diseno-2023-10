231a808b6df372028a75ec71f390d372b09cdbbf
samuelrs7 <rsrojas@uninorte.edu.co>
2023-05-03 11:45:38 -0500

    2 Vehiculo

--- static/consult.js
+++ static/consult.js
@@ -3,12 +3,13 @@ function consultar() {
   const fecha_final = document.getElementById("fecha_final").value;
   const hora_inicio = document.getElementById("hora_inicio").value;
   const hora_final = document.getElementById("hora_final").value;
+  const vehicle = document.getElementById("vehicle").value;
   if (fecha_inicio === fecha_final && hora_final < hora_inicio) {
     alert("ERROR: La hora de finalización no puede ser menor a la hora de inicio si las fechas son iguales");
     return;
   }
   
-  const url = `/consultar?fecha_inicio=${fecha_inicio}&fecha_final=${fecha_final}&hora_inicio=${hora_inicio}&hora_final=${hora_final}`;
+  const url = `/consultar?fecha_inicio=${fecha_inicio}&fecha_final=${fecha_final}&hora_inicio=${hora_inicio}&hora_final=${hora_final}&vehicle=${vehicle}`;
   fetch(url)
     .then(response => response.json())
     .then(data => {
