8df676d4a49b744ecaeff5ac7f9d3ea67816e49c
samuelrs7 <rsrojas@uninorte.edu.co>
2023-05-11 21:35:43 -0500

    Fixserver-client

--- servidor.js
+++ servidor.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(session({
   secret: 'secreto',
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
   cookie: {
     secure: false,
     httpOnly: true,
