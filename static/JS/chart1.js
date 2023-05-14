1f66a8282466a14a9bb62ab3b1391fa23780eec5
samuelrs7 <rsrojas@uninorte.edu.co>
2023-05-14 00:10:13 -0500

    Fix vehicles in real time

--- static/JS/chart1.js
+++ static/JS/chart1.js
@@ -4,26 +4,18 @@ let myChart = new Chart(ctx, {
   data: {
     labels: ['Producción estimada de CO2'],
     datasets: [{
-      label: 'Huella Total estimada Vehiculo 1',
+      label: 'Huella Total estimada',
       data: [0], // inicializamos los datos con 0
       backgroundColor: 'rgba(255, 99, 132, 0.2)',
       borderColor: 'rgba(255, 99, 132, 1)',
       borderWidth: 1
     },{
-      label: 'Producción cada 10 segundos de 8 Seres humanos (KG/CO2)',
-      data: [0],
+      label: 'Producción por hora de un Ser humano (KG/CO2)',
+      data: [0.044],
       backgroundColor: 'rgba(54, 162, 235, 0.2)',
       borderColor: 'rgba(54, 162, 235, 1)',
       borderWidth: 1
-    },{
-      label: 'Huella Total estimada  Vehiculo 2',
-      data: [0],
-      backgroundColor: 'rgba(46, 204, 113, 0.2)', // Establecer el color verde
-      borderColor: 'rgba(46, 204, 113, 1)', // Establecer el color verde
-      borderWidth: 1
-
-    } 
-  ]
+    }]
   },
   options: {
     scales: {
@@ -35,10 +27,7 @@ let myChart = new Chart(ctx, {
     }
   }
 });
-
-let totalhuella = 0;
-let seresHumanos = 0;
-let totalhuella1 = 0;
+totalhuella=0;
 setInterval(() => {
   fetch('/huella')
     .then(response => response.json())
@@ -50,4 +39,4 @@ setInterval(() => {
     .catch(error => {
       console.error(error);
     });
-}, 10000);
+}, 10000); // realizamos un fetch cada 1000 milisegundos (1 segundo)
