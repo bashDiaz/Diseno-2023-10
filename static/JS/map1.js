1d05a68802017dc188702afa8118e60947965933
samuelrs7 <rsrojas@uninorte.edu.co>
2023-05-15 22:23:01 -0500

    S

--- static/JS/map1.js
+++ static/JS/map1.js
@@ -57,7 +57,7 @@ setInterval(() => {
       updatePolyline(data.rows);
       //updateMarker(data.rows);
 });
-}, 5000);
+}, 1000);
 const zoomc = document.getElementById("sli");
 const actzoom= mymap.getZoom();
 const circle = L.circle([data1, data2], {
